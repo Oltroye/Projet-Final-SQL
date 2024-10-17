9c394f71c192ca9d84bc9172c7dbd24b49692917
cassian joly <cassian.joly@ynov.com>
2024-10-17 15:58:50 +0200

    rebuild the db

--- table.sql
+++ table.sql
@@ -1,57 +1,57 @@
 CREATE TABLE IF NOT EXISTS jobs (
-    JobUuid VARCHAR(80) PRIMARY KEY NOT NULL,
+    JobId Integer PRIMARY KEY AUTOINCREMENT NOT NULL,
     JobName VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS companies (
-    CompanyUuid VARCHAR(80) PRIMARY KEY NOT NULL,
+    CompanyId Integer PRIMARY KEY NOT NULL,
     CompanyName VARCHAR(50) NOT NULL,
     CompanySize INTEGER NOT NULL,
     Activity VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS products (
-    ProductUuid VARCHAR(80) PRIMARY KEY NOT NULL,
+    ProductId Integer PRIMARY KEY AUTOINCREMENT NOT NULL,
     ProductName VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS departements (
-    DepartementUuid VARCHAR(80) PRIMARY KEY NOT NULL,
-    CompanyUuid VARCHAR(80),
+    DepartementId Integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+    CompanyId Integer,
     DepartementName VARCHAR(30),
-    FOREIGN KEY (CompanyUuid) REFERENCES companies(CompanyUuid)
+    FOREIGN KEY (CompanyId) REFERENCES companies(CompanyId)
 );
 
 CREATE TABLE IF NOT EXISTS employees (
-    EmpoyeeUuid VARCHAR(80) PRIMARY KEY NOT NULL,
+    EmpoyeeId Integer PRIMARY KEY AUTOINCREMENT NOT NULL,
     FirstName VARCHAR(50) NOT NULL,
     LastName VARCHAR(50) NOT NULL,
-    JobUuid VARCHAR(80) NOT NULL,
+    JobId Integer NOT NULL,
     BirthDate DATE NOT NULL,
     HireDate DATE NOT NULL,
-    CompanyUuid VARCHAR(80) NOT NULL,
-    DepartementUuid VARCHAR(80) NOT NULL,
+    CompanyId Integer NOT NULL,
+    DepartementId Integer NOT NULL,
     Country VARCHAR(50) NOT NULL,
     City VARCHAR(50) NOT NULL,
     Address VARCHAR(50) NOT NULL,
     PhoneNumber VARCHAR(10) NOT NULL,
-    Senior VARCHAR(80),
+    Senior Integer,
     Status BOOLEAN NOT NULL,
-    FOREIGN KEY (CompanyUuid) REFERENCES companies(CompanyUuid),
-    FOREIGN KEY (JobUuid) REFERENCES jobs(JobUuid),
-    FOREIGN KEY (DepartementUuid) REFERENCES departements(DepartementUuid)
+    FOREIGN KEY (CompanyId) REFERENCES companies(CompanyId),
+    FOREIGN KEY (JobId) REFERENCES jobs(JobId),
+    FOREIGN KEY (DepartementId) REFERENCES departements(DepartementId)
 );
 
 CREATE TABLE IF NOT EXISTS departement_job (
-    JobUuid VARCHAR(80) NOT NULL,
-    DepartementUuid VARCHAR(80) NOT NULL,
-    FOREIGN KEY (JobUuid) REFERENCES jobs(JobUuid),
-    FOREIGN KEY (DepartementUuid) REFERENCES departements(DepartementUuid)
+    JobId Integer NOT NULL,
+    DepartementId Integer NOT NULL,
+    FOREIGN KEY (JobId) REFERENCES jobs(JobId),
+    FOREIGN KEY (DepartementId) REFERENCES departements(DepartementId)
 );
 
 CREATE TABLE IF NOT EXISTS employee_product (
-    ProductUuid VARCHAR(80) NOT NULL,
-    EmpoyeeUuid VARCHAR(80) NOT NULL,
-    FOREIGN KEY (ProductUuid) REFERENCES products(ProductUuid),
-    FOREIGN KEY (EmpoyeeUuid) REFERENCES employees(EmpoyeeUuid)
+    ProductId Integer NOT NULL,
+    EmpoyeeId Integer NOT NULL,
+    FOREIGN KEY (ProductId) REFERENCES products(ProductId),
+    FOREIGN KEY (EmpoyeeId) REFERENCES employees(EmpoyeeId)
 );
